82c8c5e43baa688de59f0775b15675e3bff7e744
Jason Forcier <jasonwforcier@gmail.com>
2014-08-29 12:19:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Realstax::Application.config.secret_key_base = 'e0998ad8b23ef39a49d0e1905e4bb8a01d4480a2cc39af823e3640735fdda55ac3d7154754c230494d050b9ea0f780f96b49351d52b9439ac55050f7d9deba95'
